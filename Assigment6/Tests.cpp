4d0b74d2365345db5b9f1a1a944c95164b8bd031
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-12-23 03:26:28 +0300

    add a tests for 1-3 Classes

--- Assigment6/Tests.cpp
+++ Assigment6/Tests.cpp
@@ -20,7 +20,7 @@ public:
 };
 
 
-TEST(MyClassTest, GenericClassFooReturnsTrue)
+TEST(MyClassTest, rTrue)
 {
     std::vector<float> v = {1.0f};
     TestClass obj;
@@ -28,7 +28,7 @@ TEST(MyClassTest, GenericClassFooReturnsTrue)
     EXPECT_TRUE(myClass.foo());
 }
 
-TEST(MyClassTest, GenericClassFooReturnsFalse)
+TEST(MyClassTest, r2False)
 {
     std::vector<float> v = {};
     TestClass obj;
@@ -36,18 +36,60 @@ TEST(MyClassTest, GenericClassFooReturnsFalse)
     EXPECT_FALSE(myClass.foo());
 }
 
-TEST(MyClassTest, SpecializationIntFooReturnsTrue)
+TEST(MyClassTest, r3True)
 {
     std::vector<float> emptyV;
     MyClass<int> myClass(1, 1, emptyV);
     EXPECT_TRUE(myClass.foo());
 }
 
-TEST(MyClassTest, SpecializationDoubleFooReturnsFalse)
+TEST(MyClassTest, r4False)
 {
     std::vector<float> emptyV;
     MyClass<double> myClass(1.1, 1.1, emptyV);
     EXPECT_FALSE(myClass.foo());
 }
 
+TEST(Class1Test, r5True) 
+{ 
+    std::vector<float> v = {1.0f};  
+    Class1 obj; 
+    EXPECT_TRUE(obj.bar(1, v)); 
+} 
+ 
+TEST(Class1Test, r6False) 
+{ 
+    std::vector<float> v = {}; 
+    Class1 obj; 
+    EXPECT_FALSE(obj.bar(-1, v));  
+} 
+ 
+TEST(Class2Test, r7True) 
+{ 
+    std::vector<float> v = {1.0f};   
+    Class2 obj; 
+    EXPECT_TRUE(obj.bar(1, v));
+} 
+ 
+TEST(Class2Test, r8False) 
+{ 
+    std::vector<float> v = {};   
+    Class2 obj; 
+    EXPECT_FALSE(obj.bar(1, v)); 
+} 
+ 
+
+TEST(Class3Test, r9True) 
+{ 
+    std::vector<float> v = {1.0f}; 
+    Class3 obj; 
+    EXPECT_TRUE(obj.bar(-1, v));  
+} 
+ 
+TEST(Class3Test, r10False) 
+{ 
+    std::vector<float> v = {1.0f}; 
+    Class3 obj; 
+    EXPECT_FALSE(obj.bar(1, v)); 
+}
 
