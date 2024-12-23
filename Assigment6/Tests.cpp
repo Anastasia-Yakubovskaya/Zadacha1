fa4098acfee4832563fbf737e3964f4838b1d7fa
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-12-23 04:04:43 +0300

    correct the error

--- Assigment6/Tests.cpp
+++ Assigment6/Tests.cpp
@@ -1,95 +1,68 @@
-/*Yakubovskaya Anastasya st130155@student.spbu.ru Zadacha6*/
-
-#include "gtest/gtest.h"
-#include "Class.h"
-#include "13Classes.h"
-
+#include "gtest/gtest.h" 
+#include "Class.h" 
+#include "13Classes.h" 
+  
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-class TestClass
-{
-public:
-    bool bar(int num, const std::vector<float>& vec)
-    {
-        return num > 0 && !vec.empty();
-    }
-};
-
-
-TEST(MyClassTest, rTrue)
-{
-    std::vector<float> v = {1.0f};
-    TestClass obj;
-    MyClass<TestClass> myClass(obj, 1, v);
-    EXPECT_TRUE(myClass.foo());
-}
-
-TEST(MyClassTest, r2False)
-{
-    std::vector<float> v = {};
-    TestClass obj;
-    MyClass<TestClass> myClass(obj, -1, v);
-    EXPECT_FALSE(myClass.foo());
-}
-
-TEST(MyClassTest, r3True)
-{
-    std::vector<float> emptyV;
-    MyClass<int> myClass(1, 1, emptyV);
-    EXPECT_TRUE(myClass.foo());
-}
-
-TEST(MyClassTest, r4False)
-{
-    std::vector<float> emptyV;
-    MyClass<double> myClass(1.1, 1.1, emptyV);
-    EXPECT_FALSE(myClass.foo());
-}
-
-TEST(Class1Test, r5True) 
+ 
+TEST(MyClassTest, rTrue) 
 { 
-    std::vector<float> v = {1.0f};  
-    Class1 obj; 
-    EXPECT_TRUE(obj.bar(1, v)); 
+    std::vector<float> v = {1.0f}; 
+    Class1 obj;  
+    MyClass<Class1> myClass(obj, 1, v);  
+    EXPECT_TRUE(myClass.foo());  
 } 
  
-TEST(Class1Test, r6False) 
+ 
+TEST(MyClassTest, r1False) 
 { 
-    std::vector<float> v = {}; 
-    Class1 obj; 
-    EXPECT_FALSE(obj.bar(-1, v));  
+    std::vector<float> emptyV; 
+    Class3 obj;  
+    MyClass<Class3> myClass(obj, -1, emptyV);  
+    EXPECT_FALSE(myClass.foo());  
 } 
  
-TEST(Class2Test, r7True) 
-{ 
+TEST(Class1Test, r1True)  
+{  
     std::vector<float> v = {1.0f};   
-    Class2 obj; 
-    EXPECT_TRUE(obj.bar(1, v));
-} 
+    Class1 obj;  
+    EXPECT_TRUE(obj.bar(1, v));  
+}  
+  
+TEST(Class1Test, r3False)  
+{  
+    std::vector<float> v = {};  
+    Class1 obj;  
+    EXPECT_FALSE(obj.bar(-1, v));  
+}  
+  
+TEST(Class2Test, r4True)  
+{  
+    std::vector<float> v = {1.0f};    
+    Class2 obj;  
+    EXPECT_TRUE(obj.bar(1, v)); 
+}  
+  
+TEST(Class2Test, r5False)  
+{  
+    std::vector<float> v = {};    
+    Class2 obj;  
+    EXPECT_FALSE(obj.bar(1, v));  
+}  
  
-TEST(Class2Test, r8False) 
-{ 
-    std::vector<float> v = {};   
-    Class2 obj; 
-    EXPECT_FALSE(obj.bar(1, v)); 
-} 
- 
-
-TEST(Class3Test, r9True) 
+TEST(Class3Test, r6True) 
 { 
     std::vector<float> v = {1.0f}; 
     Class3 obj; 
-    EXPECT_TRUE(obj.bar(-1, v));  
+    EXPECT_TRUE(obj.bar(-1, v)); 
 } 
  
-TEST(Class3Test, r10False) 
+TEST(Class3Test, r7False) 
 { 
     std::vector<float> v = {1.0f}; 
     Class3 obj; 
     EXPECT_FALSE(obj.bar(1, v)); 
 }
-
