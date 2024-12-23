cbd86ceccd9e8a8f5eeaaafb7c73c32a58d49492
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-12-23 04:22:44 +0300

    change the 13Classes

--- Assigment6/13Classes.cpp
+++ Assigment6/13Classes.cpp
@@ -1,7 +1,7 @@
 /*Yakubovskaya Anastasya st130155@student.spbu.ru Zadacha6*/
 
 #include <vector>
-
+#include <iostream>
 #include "13Classes.h"
 
 
@@ -20,7 +20,10 @@ float Class1::c_1_2()
     return 1.0f;
 }
 
-void Class1::c_1_3() {}
+void Class1::c_1_3() 
+{
+   std::cout << "method c_1_3" << std::endl; 
+}
 
 bool Class2::bar(int num, const std::vector<float>& vec)
 {
@@ -37,11 +40,14 @@ float Class2::c_2_2()
     return 2.0f;
 }
 
-void Class2::c_2_3() {}
+void Class2::c_2_3() 
+{
+    std::cout << "method c_1_2" << std::endl;
+}
 
 bool Class3::bar(int num, const std::vector<float>& vec)
 {
-    return num == -static_cast<int>(vec.size());
+    return num == (-1)*static_cast<int>(vec.size());
 }
 
 int Class3::c_3_1()
@@ -54,7 +60,10 @@ float Class3::c_3_2()
     return 3.0f;
 }
 
-void Class3::c_3_3() {}
+void Class3::c_3_3() 
+{
+    std::cout << "method c_3_3" << std::endl;
+}
 
 
 
